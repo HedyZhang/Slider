5f5c86d31142ae6c8d9f1f3cb27c74bb541cb277
张海迪 <870347271@qq.com>
2014-07-21 21:26:34 +0800

    Initial Commit

---
+++ Slider/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Slider
+//
+//  Created by 张海迪 on 14-7-21.
+//  Copyright (c) 2014年 haidi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Slider/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Slider
+//
+//  Created by 张海迪 on 14-7-21.
+//  Copyright (c) 2014年 haidi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
